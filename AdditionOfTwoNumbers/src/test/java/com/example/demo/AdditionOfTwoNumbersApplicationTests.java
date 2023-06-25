cd736088ed8dbd8e36eebf8820aabd2e0f6d1c10
jawahar-reddy <jawaharreddy.nomula@gmail.com>
2023-06-24 23:30:06 -0500

    file added

---
+++ AdditionOfTwoNumbers/src/test/java/com/example/demo/AdditionOfTwoNumbersApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;  // Import statement for MediaType
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdditionOfTwoNumbersApplicationTests {
+    
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    public void testAddNumbers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"num1\": 5, \"num2\": 10}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("15"));
+    }
+}
